dfc3355798f3c81608df9355641eefa0e9bb5446
Zack Glenn <ZackGlenn05@gmail.com>
2021-09-27 16:14:53 -0600

    Improved documentation + formatting

--- tracking_utils/tracking_interpolation.m
+++ tracking_utils/tracking_interpolation.m
@@ -2,24 +2,21 @@
 
 % Script for using cubic interpolation on tracking solutions
 % Created 8/30/21 by Zachary Glenn
-% Last modified 8/30/21 by Zachary Glenn
+% Last modified 9/27/21 by Zachary Glenn
 
 % Inputs:
 % -------
-% 1) The solution file to interpolate. Should be a csv file in the format
+%   1) The solution file to interpolate. Should be a csv file in the format
 % produced automatically by mtwtesla. Select the file in the dialog box that opens.
 %
-% This script preserves all of the existing solutions from the input file, and
+%   This script preserves all of the existing solutions from the input file, and
 % interpolates wherever there are gaps between solutions. This means it won't overwrite
 % any of the existing work, and that it will not interpolate past the bounds of the
 % existing work.
-%
-% 2) The name of the file to output to, which must be a csv file. Provide
-% the output filename as a characer vector.
 
 % Outputs:
 % --------
-% 1) A CSV file containing the cubic interpolation of the chosen solution
+%   1) A CSV file containing the cubic interpolation of the chosen solution
 % file. The output file should be fully ready for mtwtesla to read - just
 % paste it in the MTW_Solutions folder and select it from mtwtesla's
 % dropdown menu. If mtwtesla crashes after selecting the file, try using
@@ -27,21 +24,36 @@
 % it still crashes, make sure the input file is in exactly the format
 % generated automatically by mtwtesla.
 
-% Notes - the following represents my best understanding as of the time of
+% Usage:
+% ------
+%   a) Copy the solution to interpolate to the folder containing this script. Rename it
+% [Bone].csv (e.g. Scapula.csv or Hybrid.csv).
+%
+%   b) Open the script in matlab, and run it in the directory containing the script -
+% this should be the default location matlab opens to. Select [Bone].csv in the dialog
+% box that appears, and the interpolated solution file, named _[Bone]_interpolated.csv,
+% should be created.
+%
+%   c) Copy the interpolated solution to the MTW_Solutions folder for the trial. After
+% restarting MTWTesla the interpolated should appear at the top of the solutions
+% dropdown menu. You can treat interpolated solution file like any other solution.
+
+% Notes:
+% ------
+%   The following represents my best understanding as of the time of
 % writing, some of which is an educated guess on my part. Unfortunately,
 % Tesla does not provide much documentation explaining its solution files.
-% ------------------------------------------------------------------------
 %
 % 1) Cubic interpolation:
 %
-% The solutions created by this script should not be blindly trusted. As
+%   The solutions created by this script should not be blindly trusted. As
 % always, the 'garbage in, garbage out' rule applies - the interpolation is
 % only as good as the samples you provide it. It is also possible that the
 % true solution varies more quickly than the cubic interpolation can
 % capture using the data you provide it. Always check to make sure that the
 % interpolated solution accurately reflects the data seen in tracking.
 %
-% Additionally, Euler angles (i.e. the roll, pitch, and yaw values) cannot
+%   Additionally, Euler angles (i.e. the roll, pitch, and yaw values) cannot
 % be interpolated in general. The interpolation will only work correctly if
 % the solution is not too close to being singular - this is usually the
 % case, but it isn't always. Again, you must manually confirm that the
@@ -49,21 +61,21 @@
 %
 % 2) The MtwTesla solution file
 % ------------------------------
-% A solution file needs the following columns for tesla to read it and
+%   A solution file needs the following columns for tesla to read it and
 % show the plot window correctly:
 %   Frame, X, Y, Z, Roll, Pitch, Yaw, GreenCorr, RedCorr CombCorr,
 %   Iterations, RunMsec
 %
-% The Frame column lists all of the frames that have solutions. This script
+%   The Frame column lists all of the frames that have solutions. This script
 % will create solutions wherever they're missing between the first tracked
 % frame of the input and the last tracked frame of the input.
 %
-% The X, Y, Z, Roll, Pitch, and Yaw columns contain the kinematic data of
+%   The X, Y, Z, Roll, Pitch, and Yaw columns contain the kinematic data of
 % the bone for each solved frame. This section and the Frame column are
 % the important parts of what the script generates - the other columns are
 % only created so that Tesla will read and display the file correctly.
 %
-% The GreenCorr, RedCorr, and CombCorr columns are correlation values used
+%   The GreenCorr, RedCorr, and CombCorr columns are correlation values used
 % by tesla for the autotracking features. While this script does perform
 % interpolation for these values, they should not be regarded as
 % trustworthy - they usually vary too quickly for the interpolation to work
@@ -71,7 +83,7 @@
 % will automatically generate the correct values for a given frame whenever
 % a new solution is saved for that frame.
 %
-% The Iterations and RunMsec columns are for data related to Tesla's
+%   The Iterations and RunMsec columns are for data related to Tesla's
 % autotracking - the number of iterations performed and the amount of time
 % it took to perform them, respectively. In both cases, Tesla uses the
 % value -1 to indicate that autotracking wasn't used for a given frame. For
@@ -80,7 +92,7 @@
 %% Main script
 
 % clear MATLAB's workspace to avoid potential problems with pre-existing
-% variables
+% variables. Add the script to path so matlab will run it properly.
 clear
 addpath(pwd)
 
